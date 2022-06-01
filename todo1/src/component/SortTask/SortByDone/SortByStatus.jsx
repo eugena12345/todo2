fd9b1616ab044eedeccc5374679cf7f1277d18f9
eugena12345 <eugena.h12345@gmail.com>
2022-06-01 23:26:31 +0400

    add constats in 'sortByStatus'

--- todo1/src/component/SortTask/SortByDone/SortByStatus.jsx
+++ todo1/src/component/SortTask/SortByDone/SortByStatus.jsx
@@ -2,34 +2,38 @@ import React, { useState } from "react";
 import MyButton from "../../Button/MyButton";
 import style from './SortByStatus.module.css';
 
+const ALL = 'all';
+const DONE = 'done';
+const UNDONE = 'undone';
+
 const SortByStatus = (props) => {
-    const [order, setOrder] = useState('all');
+    const [order, setOrder] = useState(ALL);
 
     const orderUndone = () => {
-        props.setTypeOfSorted({ ...props.typeOfSorted, typeSortedByStatus: 'undone' })
-        setOrder('undone');
+        props.setTypeOfSorted({ ...props.typeOfSorted, typeSortedByStatus: UNDONE })
+        setOrder(UNDONE);
     };
     const orderDone = () => {
-        props.setTypeOfSorted({ ...props.typeOfSorted, typeSortedByStatus: 'done' });
-        setOrder('done');
+        props.setTypeOfSorted({ ...props.typeOfSorted, typeSortedByStatus: DONE });
+        setOrder(DONE);
         props.setCurretnPage(1);
     } 
     const orderAll = () => {
-        props.setTypeOfSorted({ ...props.typeOfSorted, typeSortedByStatus: 'all' });
-        setOrder('all');
+        props.setTypeOfSorted({ ...props.typeOfSorted, typeSortedByStatus: ALL });
+        setOrder(ALL);
         props.setCurretnPage(1);
     } 
 
     return (
         <div className={style.sortPanel}>
             <MyButton onClick={orderAll}
-                button={'all'} currentbutton={order}
+                button={ALL} currentbutton={order}
             >All</MyButton>
             <MyButton onClick={orderDone}
-                button={'done'} currentbutton={order}
+                button={DONE} currentbutton={order}
             >Done</MyButton>
             <MyButton onClick={orderUndone}
-                button={'undone'} currentbutton={order}
+                button={UNDONE} currentbutton={order}
             >Undone</MyButton>
         </div>
     )
